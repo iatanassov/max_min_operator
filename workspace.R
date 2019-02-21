cefe58d3b17f61021dd928e115840a5a491c3b89
iatanassov <iatanassov@age.mpg.de>
2019-02-21 16:18:53 +0100

    post ratpack commit

---
+++ workspace.R
@@ -0,0 +1,16 @@
+library(tercen)
+library(dplyr)
+options("tercen.serviceUri" = "http://localhost:5402/api/v1")
+options("tercen.username"= "admin")
+options("tercen.password" = "admin")
+
+options("tercen.workflowId"= "95134558b005c59a45cbb89f0f007261")
+options("tercen.stepId"= "4-5")
+
+
+(ctx = tercenCtx())  %>% 
+  select(.y, .ci, .ri) %>% 
+  group_by(.ci, .ri) %>%
+  summarise(mean = mean(.y)) %>%
+  ctx$addNamespace() %>%
+  ctx$save()
